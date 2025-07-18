d48edb53e0450d9318c859e875f393570e5c74b5
Luc1an02 <emsan9902@gmail.com>
2025-07-18 17:59:02 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,94 @@
+
+const data = [
+    // Ciclo 1
+    [
+        { codigo: "DN0101", nombre: "Introducción a la Administración de Negocios", creditos: 3 },
+        { codigo: "MA0001", nombre: "Precálculo", creditos: 0 },
+        { codigo: "MA1021", nombre: "Cálculo I para Ciencias Económicas", creditos: 4 },
+        { codigo: "EG-I", nombre: "Curso Integrado de Humanidades I", creditos: 6 },
+        { codigo: "EG-", nombre: "Curso de Arte", creditos: 2 },
+        { codigo: "RP-1", nombre: "Repertorio", creditos: 3 }
+    ],
+    // Ciclo 2
+    [
+        { codigo: "DN0102", nombre: "Aplicaciones Ofimáticas para la Toma de Decisiones", creditos: 3 },
+        { codigo: "PC0200", nombre: "Contabilidad Básica", creditos: 4 },
+        { codigo: "EG-II", nombre: "Curso Integrado de Humanidades II", creditos: 6 },
+        { codigo: "MA1022", nombre: "Cálculo II para Ciencias Económicas", creditos: 4 },
+        { codigo: "EF-", nombre: "Actividad Deportiva", creditos: 0 }
+    ],
+    // Ciclo 3
+    [
+        { codigo: "DN0103", nombre: "Administración de Proyectos y Herramientas para Análisis de Datos", creditos: 3 },
+        { codigo: "PC0202", nombre: "Contabilidad Intermedia I", creditos: 3 },
+        { codigo: "PC0240", nombre: "Matemática Financiera", creditos: 3 },
+        { codigo: "XE0156", nombre: "Introducción a la Economía", creditos: 4 },
+        { codigo: "XS0276", nombre: "Estadística General I", creditos: 4 }
+    ],
+    // Ciclo 4
+    [
+        { codigo: "DN0104", nombre: "Legislación Empresarial", creditos: 3 },
+        { codigo: "DN0123", nombre: "Metodología de la Investigación", creditos: 3 },
+        { codigo: "DN0340", nombre: "Administración Financiera I", creditos: 3 },
+        { codigo: "PC0304", nombre: "Contabilidad Intermedia II", creditos: 3 },
+        { codigo: "SR-I", nombre: "Seminario de Realidad Nacional I", creditos: 2 },
+        { codigo: "XS0277", nombre: "Estadística General II", creditos: 4 }
+    ],
+    // Ciclo 5
+    [
+        { codigo: "DN0105", nombre: "Métodos Cuantitativos I", creditos: 3 },
+        { codigo: "DN0202", nombre: "Principios de Gerencia", creditos: 3 },
+        { codigo: "DN0320", nombre: "Principios de Mercadeo", creditos: 3 },
+        { codigo: "DN0341", nombre: "Administración Financiera II", creditos: 3 },
+        { codigo: "PC0261", nombre: "Legislación Comercial", creditos: 3 },
+        { codigo: "SR-II", nombre: "Seminario de Realidad Nacional II", creditos: 2 }
+    ],
+    // Ciclo 6
+    [
+        { codigo: "DN0106", nombre: "Gestión del Talento Humano", creditos: 3 },
+        { codigo: "DN0107", nombre: "Economía y Comercio Internacional", creditos: 3 },
+        { codigo: "DN0110", nombre: "Métodos Cuantitativos II", creditos: 3 },
+        { codigo: "DN0321", nombre: "Publicidad y Promoción", creditos: 3 },
+        { codigo: "DN0442", nombre: "Administración Financiera III", creditos: 3 },
+        { codigo: "PC0260", nombre: "Legislación Laboral", creditos: 3 }
+    ],
+    // Ciclo 7
+    [
+        { codigo: "DN0304", nombre: "Liderazgo Gerencial", creditos: 3 },
+        { codigo: "DN0322", nombre: "Ventas y Distribución", creditos: 3 },
+        { codigo: "DN0405", nombre: "Emprendimiento", creditos: 3 },
+        { codigo: "DN0496", nombre: "Gerencia de Operaciones", creditos: 3 },
+        { codigo: "PC0344", nombre: "Formulación y Evaluación de Proyectos I", creditos: 3 },
+        { codigo: "PC0462", nombre: "Legislación Tributaria y Aduanera", creditos: 3 }
+    ],
+    // Ciclo 8
+    [
+        { codigo: "DN0108", nombre: "Mercados Bursátiles", creditos: 4 },
+        { codigo: "DN0109", nombre: "Principios de Auditoría", creditos: 3 },
+        { codigo: "DN0111", nombre: "Gestión de la Innovación", creditos: 3 },
+        { codigo: "DN0112", nombre: "Gerencia de la Calidad", creditos: 3 },
+        { codigo: "DN0423", nombre: "Investigación de Mercados", creditos: 3 }
+    ],
+    // Ciclo 9
+    [
+        { codigo: "DN0113", nombre: "Administración con Tecnología", creditos: 3 },
+        { codigo: "DN0124", nombre: "Costeo Gerencial", creditos: 3 },
+        { codigo: "DN0507", nombre: "Estrategias y Tácticas de Negociación", creditos: 3 },
+        { codigo: "DN0526", nombre: "Comportamiento del Consumidor", creditos: 3 },
+        { codigo: "DN0548", nombre: "Mercados Financieros Internacionales", creditos: 3 },
+        { codigo: "OPT1156", nombre: "Curso Optativo", creditos: 3 }
+    ],
+    // Ciclo 10
+    [
+        { codigo: "DN0114", nombre: "Terminología Universal", creditos: 3 },
+        { codigo: "DN0115", nombre: "Taller de Investigación", creditos: 2 },
+        { codigo: "DN0424", nombre: "Gerencia de Mercadeo", creditos: 3 },
+        { codigo: "DN0527", nombre: "Mercadeo Internacional", creditos: 3 },
+        { codigo: "DN0547", nombre: "Banca y Sistema Económico", creditos: 3 },
+        { codigo: "OPT1156", nombre: "Curso Optativo", creditos: 3 }
+    ],
+    // Ciclo 11
+    [
+        { codigo: "OPT1159", nombre: "Trabajo Final de Graduación (TFG)", creditos: 0 }
+    ]
+];
